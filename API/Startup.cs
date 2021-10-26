f11f9494daed4ecf1474f27658fcc142c5101b65
Yafet Segid <yafetsegid@gmail.com>
2021-10-25 23:30:09 -0500

    adding server

--- API/Startup.cs
+++ API/Startup.cs
@@ -10,8 +10,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-// using Microsoft.OpenApi.Models;
-using Microsoft.AspNetCore.SpaServices;
+using Microsoft.OpenApi.Models;
 
 
 
